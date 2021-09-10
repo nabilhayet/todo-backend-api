d50aa54909e34dea92da1127ef5d9bd55381e6fa
nabilhayet <nabil4457@yahoo.com>
2021-09-09 21:03:06 -0400

    added controllers for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
-  def index
+ / def index
     @users = User.all
 
     render json: @users
   end
-
+/
   # GET /users/1
   def show
     render json: @user
@@ -25,19 +25,20 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  def update
+ / def update
     if @user.update(user_params)
       render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
+/
   # DELETE /users/1
+  /
   def destroy
     @user.destroy
   end
-
+/
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -46,6 +47,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email, :password_digest)
+      params.require(:user).permit(:email, :password)
     end
 end
